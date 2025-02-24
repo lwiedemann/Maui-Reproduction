d76a514b72798cb755c0202e60a1d59d4b1c7028
Laureen Wiedemann <lwiedemann@slashwhy.de>
2025-02-24 13:34:34 +0100

    clean up
    clean up

--- Reproduction/Pages/MainPage.xaml.cs
+++ Reproduction/Pages/MainPage.xaml.cs
@@ -4,21 +4,13 @@ namespace Reproduction;
 
 public partial class MainPage
 {
-    private int _count;
-
     public MainPage()
     {
         InitializeComponent();
     }
 
-    private void OnCounterClicked(object sender, EventArgs e)
+    private void OnButtonClicked(object sender, EventArgs e)
     {
-        _count++;
-
-        CounterBtn.Text = _count == 1 ? $"Clicked {_count} time" : $"Clicked {_count} times";
-
-        SemanticScreenReader.Announce(CounterBtn.Text);
-        
         Navigation.PushAsync(new ReproductionPage(), true);
     }
 }
